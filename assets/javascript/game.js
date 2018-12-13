2af0e243eda1cd9601c62d0d346fc3a93afd5041
Rodolfo <jrsotorodolfo@gmail.comgit config --global user.email jrsotorodolfo@gmail.comgit config --global user.email jrsotorodolfo@gmail.com>
2018-12-12 20:43:26 -0600

    before thinking about redoing all js code

---
+++ assets/javascript/game.js
@@ -0,0 +1,53 @@
+//Create choices
+var wordChoice = ['rick', 'walker', 'hershel'];
+var randomArray = Math.floor(Math.random() * wordChoice.length);
+var randomWord = wordChoice[randomArray];
+
+// console.log(randomWord)
+var wordGuess = document.getElementById("newWord");
+
+//Place underscores for letter position
+
+var underScore = [];
+
+function replaceUnderScore() {
+    for (var i = 0; i < randomWord.length; i++) {
+        underScore.push('_');
+    }
+    return underScore;
+
+}
+
+wordGuess.textContent = replaceUnderScore();
+
+console.log(underScore);
+
+
+//Initiate game by pressing a key
+
+document.onkeyup = function (event) {
+
+    var userGuess = event.key;
+
+    if(userGuess === "r");
+    
+
+    console.log (userGuess);
+}
+
+// var userGuess = 0;
+// document.onkeyup = function (event) {
+//     var userGuess = event.key;
+//     if (userGuess) === 
+//     console.log (userGuess)
+//     console.log(this.onkeyup);
+// }
+
+
+//When user presses key determine if key is part of the word
+//If it is not part of the word, guessesRemaining goes down by 1
+//After a missed guess by user, the letter already used is displayed
+//If it is part of the word, override an underscore with the letter chosen
+//If the user runs out of guesses, game ends
+//If user chooses all correct letter, user wins
+
